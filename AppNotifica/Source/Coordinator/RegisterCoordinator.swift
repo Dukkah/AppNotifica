adac48f563a27b253fb14099e58b0258c385bfa4
Dukkah <106485047+Dukkah@users.noreply.github.com>
2023-06-20 11:02:23 -0300

    video-5

---
+++ AppNotifica/Source/Coordinator/RegisterCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  RegisterCoordinator.swift
+//  AppNotifica
+//
+//  Created by IFBIOTIC14 on 20/06/23.
+//
+
+import Foundation
+
+
+import Foundation
+import UIKit
+class RegisterCoordinator: Coordinator {
+
+    var navigationController: UINavigationController
+    
+    init (navigationController: UINavigationController ) {
+            self.navigationController = navigationController
+     
+        }
+
+      func start() {
+        let viewController = RegisterViewController()
+            self.navigationController.pushViewController(viewController, animated: true)
+
+    }
+}
